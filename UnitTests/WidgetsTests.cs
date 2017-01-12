4b3b70b1367b3207c85e3357efb9bd25f80c2926
Jake Garner <jacob.garner@dealertrack.com>
2017-01-11 21:18:34 -0700

    Add unit test project.

---
+++ UnitTests/WidgetsTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetUserGroupDemo.APIControllers;
+using NetUserGroupDemo.Models;
+using NetUserGroupDemo.Repository;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class WidgetsApiControllerTests
+    {
+        [TestMethod]
+        public void Widgets_ReturnsTrue_When()
+        {
+            // Arrange
+            WidgetsController controller = new WidgetsController(new DAL(new Logger<DAL>(new LoggerFactory())), new Logger<WidgetsController>(new LoggerFactory()));
+            var expected = new Widget() { Id = 1, Name = "Widget 1", Price = 99.99m };
+
+            // Act
+            var actual = controller.Get().First();
+
+            // Assert
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.Name, actual.Name);
+            Assert.AreEqual(expected.Price, actual.Price);
+        }
+    }
+}
--- src/NetUserGroupDemo/APIControllers/WidgetsController.cs
+++ src/NetUserGroupDemo/APIControllers/WidgetsController.cs
@@ -1,46 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+using Microsoft.Extensions.Logging;
+using NetUserGroupDemo.Models;
+using NetUserGroupDemo.Repository;
 
 namespace NetUserGroupDemo.APIControllers
 {
     [Route("api/[controller]")]
     public class WidgetsController : Controller
     {
+        private readonly IDAL _dal;
+        private readonly ILogger<WidgetsController> _logger;
+
+        public WidgetsController(IDAL dal, ILogger<WidgetsController> logger)
+        {
+            _dal = dal;
+            _logger = logger;
+        }
+
         // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Widget> Get()
         {
-            return new string[] { "value1", "value2" };
+            _logger.LogError("In API::WidgetsController::Get Method");
+            return new Widget[] { _dal.GetWidget()};
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Widget Get(int id)
         {
-            return "value";
+            _logger.LogError($"In API::WidgetsController::Get by id Method: id = {id}");
+            return _dal.GetWidget();
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
         {
+            throw new NotImplementedException();
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
+            throw new NotImplementedException();
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            throw new NotImplementedException();
         }
     }
 }
